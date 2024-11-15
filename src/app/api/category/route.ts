fb08629e4fbe230440b1372b62f7470eb3b9a718
Parthiv <parmarparthiv77@gmail.com>
2024-11-15 22:57:27 +0530

    add: dashboard types

--- src/app/api/category/route.ts
+++ src/app/api/category/route.ts
@@ -1,10 +1,35 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getEventCategories } from '../__internals/get-event-catergories';
 
+type Category = {
+  uniqueFieldCount: number;
+  eventsCount: number;
+  lastPing: Date | null;
+  id: string;
+  name: string;
+  color: string;
+  emoji: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+type CategoryResponse = {
+  categories: Category[];
+};
+
 export async function GET(req: NextRequest) {
   try {
     const response = await getEventCategories(req);
-    return NextResponse.json(response);
+    const json = await response.json();
+    if ('categories' in json) {
+      const { categories }: CategoryResponse = json;
+      return NextResponse.json({ categories });
+    } else {
+      return NextResponse.json(
+        { error: 'Invalid response structure' },
+        { status: 500 }
+      );
+    }
   } catch (error) {
     return NextResponse.json(
       { error: 'Error syncing with the database' },
--- src/app/dashboard/dashboard-page-content.tsx
+++ src/app/dashboard/dashboard-page-content.tsx
@@ -1,13 +1,31 @@
+'use client';
+
 import { useQuery } from '@tanstack/react-query';
 import { apiGet } from '../utils/apiClient';
+import { LoadingSpinner } from '@/components/loading-spinner';
 
 export const DashboardPageContent = () => {
-  const { data } = useQuery({
+  const { data: categories, isPending: isLoadingCategories } = useQuery({
     queryFn: async () => {
       const res = await apiGet('/api/category');
-      // console.log(data);
       return res;
     },
     queryKey: ['user-event-categories'],
   });
+
+  if (isLoadingCategories) {
+    return (
+      <div className="flex h-full w-full flex-1 items-center justify-center">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  return (
+    <ul className="grid max-w-6xl grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3">
+      {categories.map((category) => {
+        <li key={category.id}></li>;
+      })}
+    </ul>
+  );
 };
